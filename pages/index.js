caf338296dbb3dccad03a9bd447331ace5c07fea
Carl-Magnus Lein <carl@live.no>
2021-12-09 09:15:21 +0100

    Assignemnt 3 for unfold tests

--- pages/index.js
+++ pages/index.js
@@ -1,100 +1,117 @@
 import Head from 'next/head'
+import {useState} from "react"
 
 export default function Home() {
+  const [imageOverlayOpen, setImageOverlayOpen] = useState(false);
+
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Assignment 3</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <p className={imageOverlayOpen ? "":"title"}><b>Shared</b> by you</p>
+        <img className={imageOverlayOpen && "open"} src="tokyo.jpeg"/>
+        <div className={imageOverlayOpen && "titleOpen"}>
+          <h2>Lost in Tokyo</h2>
+          {imageOverlayOpen && <p style={{fontSize: 20, textAlign: "left"}}>By Jonathan Vik</p>}
         </div>
+        <p style={{lineHeight: 1.7}}>Cras mattis consectetur purus sit amet fermentum. 
+          Maecenas sed diam eget risus varius blandit sit amet non magna
+        </p>
+        <button 
+          className={`openButton ${imageOverlayOpen && "closeButton"}`} 
+          onClick={() => {
+            setImageOverlayOpen(!imageOverlayOpen);
+          }}
+        >{imageOverlayOpen ? "X" : "<"}</button>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
         .container {
           min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
         }
 
         main {
-          padding: 5rem 0;
           flex: 1;
           display: flex;
           flex-direction: column;
-          justify-content: center;
+          justify-content: space-evenly;
           align-items: center;
+          height: 100vh; 
         }
 
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
+        main p{
+          width: 300px;
+          text-align: center;
         }
 
-        footer img {
-          margin-left: 0.5rem;
+        img{
+          object-fit: cover;
+          width: 80vw;
+          height: 30vh;
+          margin-top: 40px;
+          border-radius: 4px;
+          transition: all 0.7s ease;
+          -webkit-transition: all 0.7s ease;
         }
 
         footer a {
           display: flex;
           justify-content: center;
           align-items: center;
+        }  
+
+        .title {
+          position: absolute;
+          top: 40px;
+        }
+
+        h2 {
+          font-size: 40px;
+        }
+
+        .titleOpen {
+          margin: 0;
+          position: fixed;
+          left: 5;
+          bottom: 40px;
+          z-index: 1;
+          color: white;
+          width: 300px;
+          font-size: 40px;
+        }
+
+        .open{
+          position: fixed;
+          width: 100vw; 
+          height: 100vh;
+          z-index: 0;
+          object-fit: cover;
+          margin: 0;
+          border-radius: 0px;
+        }
+
+        .openButton{
+          display:block;
+          height: 60px;
+          width: 60px;
+          border-radius: 50%;
+          background-color: black;
+          color: white;
+          border: none;
+        }
+        
+        .closeButton{
+          height: 40px;
+          width: 40px;
+          position: fixed;
+          top: 5px;
+          background-color: transparent; 
+          opacity: 0.7;
+          left: 5px;
         }
 
         a {
@@ -102,92 +119,6 @@ export default function Home() {
           text-decoration: none;
         }
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
       `}</style>
 
       <style jsx global>{`
@@ -198,6 +129,11 @@ export default function Home() {
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
+        }
+
+        h2,
+        p {
+          margin: 0;
         }
 
         * {
